48af4e568d9a5dba8ba852e74b2caeb543b0efea
arpitaghetiya <arpitaghetiya@gmail.com>
2026-08-27 18:51:15 +0530

    Add DataTypes

---
+++ Basics/Things to Know/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int Num=10;
+        float Num2=3.14F;
+        double Num3=10.47;
+        char ch='A';
+        boolean value= true;
+        String name="Hello";
+        long Num4=1000000;
+        System.out.println("Integer value: " +Num);
+        System.out.println("Float value: " +Num2);
+        System.out.println("Double value: " +Num3);
+        System.out.println("Character value: " +ch);
+        System.out.println("Boolean value: " +value);
+        System.out.println("String value: " +name);
+        System.out.println("Long value: " +Num4);
+    
+    }
+}
