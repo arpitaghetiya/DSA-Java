7cc845c9f1810004dfd8454339f3facebb9364b8
arpitaghetiya <arpitaghetiya@gmail.com>
2026-08-27 19:27:42 +0530

    Add Strings

---
+++ Basics/Things to Know/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str="Hello World";
+        System.out.println("String is: " +str);
+        System.out.println("Length of String is: " +str.length());
+        System.out.println("Character at index 4 is: " + str.charAt(4));
+        System.out.println("String in Uppercase is: "+ str.toUpperCase());
+        System.out.println("String in Lowercase is: "+ str.toLowerCase());
+    }
+}
