c56d3ee6db5c9454bd5f89439e0b00dca7c52779
arpitaghetiya <arpitaghetiya@gmail.com>
2026-08-30 19:52:03 +0530

    Add Pattern1

---
+++ Basics/Patterns/Pattern1.java
@@ -0,0 +1,12 @@
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++){
+            System.out.println();
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+        }
+    }
+}
