0344c90661ce4558d127b40e9570204a2179cd05
arpitaghetiya <arpitaghetiya@gmail.com>
2026-09-06 19:41:42 +0530

    Add Pattern 8

---
+++ Basics/Patterns/Pattern8.java
@@ -0,0 +1,14 @@
+public class Pattern8 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0; i<=n-1; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("  ");
+            }
+            for(int k=2*n-1-2*i; k>=1; k--){                
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
