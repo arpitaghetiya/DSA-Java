2f42345a3c0a39493006ec82a17326dc9bdd84bd
arpitaghetiya <arpitaghetiya@gmail.com>
2026-09-07 19:18:06 +0530

    Add Pattern 10

---
+++ Basics/Patterns/Pattern10.java
@@ -0,0 +1,17 @@
+public class Pattern10 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0; i<=n-1; i++){
+            for(int j=1; j<=i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<=n-1; i++){
+            for(int j=n-2; j>=i-1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
