f8625b473dd4596ae3e745ee363d1edca8c281d3
arpitaghetiya <arpitaghetiya@gmail.com>
2026-09-07 19:01:38 +0530

    Add Pattern 9

---
+++ Basics/Patterns/Pattern9.java
@@ -0,0 +1,23 @@
+    public class Pattern9 {
+        public static void main(String[] args){
+            int n=5;
+            for(int i=1; i<=n; i++){
+                for(int j=n-i; j>=0; j--){
+                    System.out.print("  ");
+                }
+                for(int k=1; k<=i*2-1; k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            for(int i=0; i<=n-1; i++){
+                for(int j=0; j<=i; j++){
+                    System.out.print("  ");
+                }
+                for(int k=n*2-1; k>=i*2+1; k--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
